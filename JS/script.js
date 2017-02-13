14815fedc1aaefed223aacfb80dd68639a5617dd
Jipsian <sylvain.labarre@live.fr>
2017-02-13 19:50:01 +0100

    OK, à fix : ancres + envoie e mail

---
+++ JS/script.js
@@ -0,0 +1,66 @@
+/*Animation skills*/
+
+$(function() {
+    var divs = $(".skills");
+    divs.not("#skill1").hide();
+    $("a").click(function() {
+        divs.filter(":visible").hide();
+        $($(this).attr("href")).show();
+        return false;
+    });
+});
+
+
+/*Animations PortFolio*/
+
+$(function() {
+    var filterList = {
+        init: function() {
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                onMixEnd: filterList.hoverEffect()
+            });
+        },
+        hoverEffect: function() {
+            $('#portfoliolist .portfolio').hover(
+                function() {
+                    $(this).find('.label').stop().animate({
+                        bottom: 0
+                    }, 200, 'easeOutQuad');
+                    $(this).find('img').stop().animate({
+                        top: -30
+                    }, 500, 'easeOutQuad');
+                },
+                function() {
+                    $(this).find('.label').stop().animate({
+                        bottom: -40
+                    }, 200, 'easeInQuad');
+                    $(this).find('img').stop().animate({
+                        top: 0
+                    }, 300, 'easeOutQuad');
+                }
+            );
+        }
+    };
+    filterList.init();
+});
+
+
+/*Modale ML*/
+
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus();
+});
+
+
+/* Ancres */
+
+$('a[href^="#"]').click(function(){
+    var id = $(this).attr("href");
+    var offset = $(id).offset().top
+    $('html, body').animate({scrollTop: offset}, 'slow');
+    return false;
+});
